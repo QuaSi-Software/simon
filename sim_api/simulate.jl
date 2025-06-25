81af334175cb18fd6da1c2b4aa8e4e8f8e645266
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-06-25 15:55:43 +0200

    Update testing simulation script for more output.

--- sim_api/simulate.jl
+++ sim_api/simulate.jl
@@ -1,5 +1,31 @@
+using JSON
+
 function main()
-    println("hello world")
+    if length(ARGS) < 1
+        println("Error: No config filename given")
+        return
+    end
+
+    filename = ARGS[1]
+    if !isfile(filename)
+        println("Error: Could not find config file '$filename'")
+        return
+    end
+
+    try
+        json_data = JSON.parsefile(filename)
+        output_filename = "output.txt"
+        open(output_filename, "w") do file
+            for (key, value) in json_data
+                println(file, "$key: $value")
+            end
+        end
+    catch e
+        println("Error: Could not parse JSON in config file: $e")
+        return
+    end
+
+    println("Success: Simulation complete")
 end
 
 main()
