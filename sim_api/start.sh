f8c7e220fb6bb32f6892245d038d53f5c1140145
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-07-17 11:51:08 +0200

    Fix missing julia environment parameter in starting scanner.

--- sim_api/start.sh
+++ sim_api/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 julia -e 'using Pkg; Pkg.activate("."); Pkg.add(["Dates", "Printf", "JSON"]);'
-julia --threads=$SIM_NR_THREADS ./scanner.jl &
+julia --threads=$SIM_NR_THREADS --project=. ./scanner.jl &
 flask run --port 5000 &
 wait
