ccdfc4a8d2d2ad93f39b7b43c1bd1fb621549181
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-07-22 10:22:27 +0200

    Implement checking run status after starting simulation in webapp.

--- webapp/simon_webapp/static/main.js
+++ webapp/simon_webapp/static/main.js
@@ -1,7 +1,33 @@
 API_ROOT = "http://127.0.0.1:5001/"
 
+run_status = {}
+
 function by_id(id) {
     return document.getElementById(id)
+}
+
+async function check_status(run_id) {
+    console.log("check_status called")
+    let response = await fetch(
+        API_ROOT + 'run_status/' + run_id,
+        {method: 'GET'}
+    )
+    let result = await response.json()
+
+    run_status["status"] = result["code"]
+    by_id('run-status').innerText = run_status["status"]
+
+    let item = '<div class="query-result card"><div class="card-body">'
+            + '<span>Request: run_status</span><br/>'
+            + '<span>Code: ' + (response.status ? response.status : 'Error') + '</span><br/>'
+            + '<span>Message: ' + (result["message"] ? result["message"] : '') + '</span><br/>'
+            + '<span>Error: ' + (result["error"] ? result["error"] : '') + '</span>'
+            + '</div></div>'
+    by_id('query-list').innerHTML = item + by_id('query-list').innerHTML
+
+    if (run_status["status"] === "finished") {
+        clearInterval(run_status["interval_id"])
+    }
 }
 
 function main() {
@@ -15,7 +41,8 @@ function main() {
         })
         let result = await response.json()
 
-        by_id('run-id').innerText = result["run_id"]
+        run_status["run_id"] = result["run_id"]
+        by_id('run-id').innerText = run_status["run_id"]
 
         let item = '<div class="query-result card"><div class="card-body">'
             + '<span>Request: start_simulation</span><br/>'
@@ -24,6 +51,11 @@ function main() {
             + '<span>Error: ' + (result["error"] ? result["error"] : '') + '</span>'
             + '</div></div>'
         by_id('query-list').innerHTML = item + by_id('query-list').innerHTML
+
+        if (run_status["interval_id"]) {
+            clearInterval(run_status["interval_id"]);
+        }
+        run_status["interval_id"] = setInterval(check_status, 10 * 1000, run_status["run_id"])
     }
 }
 
