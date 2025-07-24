46eb27f135f1f9fad625918db15bf4e07b01ccf9
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-07-24 11:29:33 +0200

    Implement fetching and displaying simulation results.
    For the time being this is simply an image that is displayed.

--- webapp/simon_webapp/static/main.js
+++ webapp/simon_webapp/static/main.js
@@ -6,8 +6,33 @@ function by_id(id) {
     return document.getElementById(id)
 }
 
+async function fetch_results(run_id) {
+    let response = await fetch(API_ROOT + 'fetch_results/' + run_id, {method: 'GET'})
+    let result = {}
+    if (response.status >= 400) {
+        result = response.json()
+    }
+    let item = '<div class="query-result card"><div class="card-body">'
+            + '<span>Request: fetch_results</span><br/>'
+            + '<span>Code: ' + (response.status ? response.status : 'Error') + '</span><br/>'
+            + '<span>Message: ' + (result["message"] ? result["message"] : '') + '</span><br/>'
+            + '<span>Error: ' + (result["error"] ? result["error"] : '') + '</span>'
+            + '</div></div>'
+    by_id('query-list').innerHTML = item + by_id('query-list').innerHTML
+
+    let blob = await response.blob();
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(blob);
+    img.alt = 'Simulation Result';
+    img.className = 'simulation-results';
+    img.width = 900;
+    img.height = 533;
+    let results_div = by_id('simulation-results');
+    results_div.innerHTML = '';
+    results_div.appendChild(img);
+}
+
 async function check_status(run_id) {
-    console.log("check_status called")
     let response = await fetch(
         API_ROOT + 'run_status/' + run_id,
         {method: 'GET'}
@@ -27,6 +52,7 @@ async function check_status(run_id) {
 
     if (run_status["status"] === "finished") {
         clearInterval(run_status["interval_id"])
+        await fetch_results(run_id)
     }
 }
 
