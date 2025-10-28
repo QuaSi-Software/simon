5d8dcf942d554ee37d3553a85c2bdb478317aef6
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-10-28 13:01:12 +0100

    Add comment explaining a problem mit CRLF in starting script.

--- sim_api/start.sh
+++ sim_api/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# when you checkout this file on windows, make sure to convert from CRLF to LF because
+# otherwise the script will not work inside the docker container
 julia -e 'using Pkg; Pkg.activate("."); Pkg.add(["Dates", "Printf", "JSON", "Plots", "UUIDs"]);'
 julia --threads=$SIM_NR_THREADS --project=. ./scanner.jl &
 flask run --port 5000 &
