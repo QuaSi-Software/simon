4301d9e66b2bc6820e1dab7f20c422ed8dd45451
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-10-29 17:04:44 +0100

    Fix issue with julia package Makie not being able to be compiled due to the headless environment of a docker container.

--- sim_api/start.sh
+++ sim_api/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # when you checkout this file on windows, make sure to convert from CRLF to LF because
 # otherwise the script will not work inside the docker container
-julia -e 'using Pkg; Pkg.activate("."); Pkg.add(["Dates", "Printf", "JSON", "Plots", "UUIDs"]);'
+
+# because docker containers are headless, julia package Makie will not compile, complaining
+# about a missing display. this issue is solved by using a virtual display provided by
+# apt package xvfb
+DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' julia --threads=$SIM_NR_THREADS -e 'using Pkg; Pkg.activate("."); Pkg.develop(Pkg.PackageSpec(; path="./resie/")); Pkg.add(["Dates", "JSON", "Printf", "UUIDs"]);'
 julia --threads=$SIM_NR_THREADS --project=. ./scanner.jl &
 flask run --port 5000 &
 wait
