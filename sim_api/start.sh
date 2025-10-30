dbbea7d6ee69575dfcad5cd1448074425f14a84c
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-10-30 11:29:21 +0100

    Update simulation code with using package Resie instead of calculating Julia sets.
    For the moment the Resie simulation is just a placeholder.

--- sim_api/start.sh
+++ sim_api/start.sh
@@ -5,7 +5,7 @@
 # because docker containers are headless, julia package Makie will not compile, complaining
 # about a missing display. this issue is solved by using a virtual display provided by
 # apt package xvfb
-DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' julia --threads=$SIM_NR_THREADS -e 'using Pkg; Pkg.activate("."); Pkg.develop(Pkg.PackageSpec(; path="./resie/")); Pkg.add(["Dates", "JSON", "Printf", "UUIDs"]);'
-julia --threads=$SIM_NR_THREADS --project=. ./scanner.jl &
+DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' julia --threads=$SIM_NR_THREADS -e 'using Pkg; Pkg.activate("."); Pkg.develop(Pkg.PackageSpec(; path="./resie/")); Pkg.add(["Dates", "JSON", "Printf", "UUIDs", "Logging"]); Pkg.precompile();'
+DISPLAY=:0 xvfb-run -s '-screen 0 1024x768x24' julia --threads=$SIM_NR_THREADS --project=. ./scanner.jl &
 flask run --port 5000 &
 wait
